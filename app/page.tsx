64db8c90279d4980a7570e352322c0fa1c3519f9
Ubangha1 <ubanghaewa@gmail.com>
2024-05-08 13:38:15 +0100

    first commit, user authentication with clerk implemented

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
+export default function Page() {
+  return (
+    <div> 
+      This page can only be seen by authenticated users
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
+}
